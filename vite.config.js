2db30f6933cbe69c6e9818d1eedb21ef61f10342
Grace Wang <t4_23@yahoo.com.tw>
2023-03-08 14:59:08 +0800

    swiper RWD

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/2023-vue3-GoodVegan/',
+  base: '/vue3-QA/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
